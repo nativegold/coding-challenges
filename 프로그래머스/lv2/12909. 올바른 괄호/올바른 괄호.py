9bfae6322b4ea14ef28803c513e237667dbbc718
Dongwoon Kim <kdw0410@outlook.com>
2023-05-27 23:26:35 +0900

    [level 2] Title: 올바른 괄호, Time: 7.68 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,19 @@
+def solution(s):
+    answer = True
+    
+    start = 0
+    end = 0
+    
+    for c in s:
+        if(start < end):
+            return False
+        
+        if(c == '('):
+            start += 1
+        else:
+            end += 1
+    
+    if start != end:
+        return False
+    else:
+        return True
