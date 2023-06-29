d7cde20ad865278fa4c938114bc8838e92c64cb8
Dongwoon Kim <kdw0410@outlook.com>
2023-06-29 23:27:43 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,17 @@
+from math import floor
+
+def solution(price):
+    answer = 0
+    
+    if price >= 500000:
+        answer += price - price * 0.2
+    elif price >= 300000:
+        answer += price - price * 0.1
+    elif price >= 100000:
+        answer += price - price * 0.05
+    else:
+        answer = price
+    
+    answer = floor(answer)
+    
+    return answer
