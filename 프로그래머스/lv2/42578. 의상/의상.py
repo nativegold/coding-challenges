f3f29e67f49757ee485b11914ea140e90e4fbe4b
Dongwoon Kim <kdw0410@outlook.com>
2023-07-02 17:26:44 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.py
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -2,13 +2,16 @@ def solution(clothes):
     result = 1
     clothes_count = {}
     
+    # 의상 종류별 갯수로 딕셔너리 만들기
     for c in clothes:
         if c[1] not in clothes_count:
             clothes_count[c[1]] = 1
         else:
             clothes_count[c[1]] += 1
     
+    # 의상 갯수로 경우의 수 구하기(입지 않을 경우 포함)
     for cnt in clothes_count.values():
         result *= cnt + 1
-        
+    
+    # 의상을 입지 않은 경우 제외하고 결과 반환
     return result - 1
