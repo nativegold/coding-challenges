95f4df5fb351c80dd3331402c1d078e89db48c66
Dongwoon Kim <kdw0410@outlook.com>
2023-07-02 17:23:57 +0900

    [level 2] Title: 전화번호 목록, Time: 202.64 ms, Memory: 34.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,12 +1,14 @@
 def solution(phone_book):
     pb_hash = {}
     
+    # 딕셔너리를 사용해 해시 만들기
     for pn in phone_book:
         pb_hash[pn] = True
     
     for pn in phone_book:
         finding = ''
         
+        # 번호를 하나씩 가져오면서 접두어인 번호 찾기
         for n in pn:
             finding += n
             if finding in pb_hash and finding != pn:
