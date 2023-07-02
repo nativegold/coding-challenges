a9b8bf1702f772f24fb6d3557ab8a7ea9e978510
Dongwoon Kim <kdw0410@outlook.com>
2023-07-02 17:29:41 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,7 +1,10 @@
 def solution(brown, yellow):
+    # 전체 넓이
     area = brown + yellow
     
+    # 카펫의 가로 길이(x), 카펫의 세로 길이(y)
     for x in range(1, area):
+        # 가로 길이로 넓이가 나누어질 때
         if (area / x) % 1 == 0:
             y = area / x
             
