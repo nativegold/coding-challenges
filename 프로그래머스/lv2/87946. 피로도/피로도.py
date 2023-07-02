ea819d2002eb57cdfb5d68db7150b74bdea76ef2
Dongwoon Kim <kdw0410@outlook.com>
2023-07-02 17:30:59 +0900

    [level 2] Title: 피로도, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -1,12 +1,15 @@
 from itertools import permutations
 
 def solution(k, dungeons):
+    # 최대 던전 수
     answer = 0
     
+    # 순열 사용
     for pm in permutations(dungeons, len(dungeons)):
         cnt = 0
         fatique = k
         
+        # 경우 탐색
         for need, use in pm:
             if fatique >= need:
                 fatique -= use
