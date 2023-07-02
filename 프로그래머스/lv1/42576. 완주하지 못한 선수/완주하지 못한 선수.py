86985c709acf3eef62032b921872e047359787c8
Dongwoon Kim <kdw0410@outlook.com>
2023-07-02 17:22:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 77.38 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,6 +1,8 @@
 from collections import Counter
 
 def solution(participant, completion):
+    # Counter 객체 뺄셈으로 동일하지 않은 부분 찾기
     answer = Counter(participant) - Counter(completion)
+    
     return list(answer.keys())[0]
     
