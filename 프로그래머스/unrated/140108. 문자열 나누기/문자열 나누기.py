6b72d4a36e128490d26b0d20f0f495997f98cfd0
Dongwoon Kim <kdw0410@outlook.com>
2023-07-02 17:55:29 +0900

    [unrated] Title: 문자열 나누기, Time: 0.80 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = 0
+    sav1=0
+    sav2=0
+    for i in s:
+        if sav1==sav2:
+            answer+=1
+            a=i
+        if i==a:
+            sav1+=1
+        else:
+            sav2+=1
+    return answer
