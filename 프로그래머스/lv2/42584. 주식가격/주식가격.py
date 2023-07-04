80ce4ae76ed476fd2e2cdb7e397d8dd9d56c0d72
Dongwoon Kim <kdw0410@outlook.com>
2023-07-04 21:41:52 +0900

    [level 2] Title: 주식가격, Time: 20.62 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,21 +1,3 @@
-from collections import deque
-
 def solution(prices):
-    stack = deque([(0, prices[0])])
-    result = [0] * len(prices)
-    sec = 0
-    
-    for i in range(1, len(prices)):
-        sec += 1
-        
-        while stack and stack[-1][1] > prices[i]:
-            price_index = stack.pop()[0]
-            result[price_index] = sec - price_index
-        
-        stack.append((i, prices[i]))
-    
-    while stack:
-        price_index = stack.pop()[0]
-        result[price_index] = sec - price_index
-        
-    return result
+    answer = []
+    return answer
