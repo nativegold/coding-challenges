a3dcb92f958926fd977e4046480c2564f7633b9e
Dongwoon Kim <kdw0410@outlook.com>
2023-07-05 02:26:10 +0900

    [level 1] Title: 모의고사, Time: 5.52 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,6 +1,6 @@
 def solution(answers):
     answer = []
-    correct_count = [[i, 0] for i in range(1, 4)]
+    score = [0] * 3
     index = [0] * 3
     way1 = [1, 2, 3, 4, 5]
     way2 = [2, 1, 2, 3, 2, 4, 2, 5]
@@ -8,11 +8,11 @@ def solution(answers):
     
     for ans in answers:
         if ans == way1[index[0]]:
-            correct_count[0][1] += 1
+            score[0] += 1
         if ans == way2[index[1]]:
-            correct_count[1][1] += 1
+            score[1] += 1
         if ans == way3[index[2]]:
-            correct_count[2][1] += 1
+            score[2] += 1
         
         index[0] += 1
         index[1] += 1
@@ -25,15 +25,9 @@ def solution(answers):
         if index[2] == len(way3):
             index[2] = 0
     
-    correct_count.sort(reverse=True, key=lambda x:x[1])
-    max_count = 0
-    
-    for stu, cnt in correct_count:
-        if max_count < cnt:
-            max_count = cnt
-        
-        if max_count == cnt:
-            answer.append(stu)
+    for i, s in enumerate(score):
+        if s == max(score):
+            answer.append(i+1)
             
-    return sorted(answer)
+    return answer
     
