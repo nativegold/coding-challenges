65b909469fccf943e742333a7b3551eb7018b332
Dongwoon Kim <kdw0410@outlook.com>
2023-07-04 21:42:07 +0900

    [level 2] Title: 주식가격, Time: 20.62 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/README.md
+++ 프로그래머스/lv2/42584. 주식가격/README.md
@@ -1,6 +1,6 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584#) 
 
 ### 성능 요약
 
--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,3 +1,21 @@
+from collections import deque
+
 def solution(prices):
-    answer = []
-    return answer
+    stack = deque([(0, prices[0])])
+    result = [0] * len(prices)
+    sec = 0
+    
+    for i in range(1, len(prices)):
+        sec += 1
+        
+        while stack and stack[-1][1] > prices[i]:
+            price_index = stack.pop()[0]
+            result[price_index] = sec - price_index
+        
+        stack.append((i, prices[i]))
+    
+    while stack:
+        price_index = stack.pop()[0]
+        result[price_index] = sec - price_index
+        
+    return result
