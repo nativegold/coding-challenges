2614a85e2484bba2447ed7a8e678cbf3d5d008cd
Dongwoon Kim <kdw0410@outlook.com>
2023-07-15 19:48:39 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -4,36 +4,36 @@ def solution(fees, records):
     answer = []
     in_time = {}
     parking_time = {}
-    last_sec = 23 * 60 + 59
+    last_minutes = 23 * 60 + 59
     
     for record in records:
         time, number, state = record.split(' ')
         
-        sec = int(time[3:5]) + int(time[0:2]) * 60
+        minutes = int(time[3:5]) + int(time[0:2]) * 60
         
         if state == 'IN':
-            in_time[number] = sec
+            in_time[number] = minutes
         else:
             if number in parking_time:
-                parking_time[number] += sec - in_time[number]
+                parking_time[number] += minutes - in_time[number]
                 in_time.pop(number)
             else:
-                parking_time[number] = sec - in_time[number]
+                parking_time[number] = minutes - in_time[number]
                 in_time.pop(number)
     
-    for number, in_sec in in_time.items():
+    for number, in_minutes in in_time.items():
         if number in parking_time:
-            parking_time[number] += last_sec - in_sec
+            parking_time[number] += last_minutes - in_minutes
         else:
-            parking_time[number] = last_sec - in_sec
+            parking_time[number] = last_minutes - in_minutes
     
     pt_array = list(parking_time.items())
     pt_array.sort(key=lambda x:x[0])
     
-    for number, pt_sec in pt_array:
-        if pt_sec <= fees[0]:
+    for number, pt_minutes in pt_array:
+        if pt_minutes <= fees[0]:
             answer.append(fees[1])
         else:
-            answer.append(fees[1] + ceil((pt_sec - fees[0]) / fees[2]) * fees[3])
+            answer.append(fees[1] + ceil((pt_minutes - fees[0]) / fees[2]) * fees[3])
             
     return answer
