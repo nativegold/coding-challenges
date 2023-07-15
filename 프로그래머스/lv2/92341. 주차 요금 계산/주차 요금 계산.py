b0f1f83404a2d610471307aeba560d7e9a310a08
Dongwoon Kim <kdw0410@outlook.com>
2023-07-15 19:51:43 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -2,18 +2,23 @@ from math import ceil
 
 def solution(fees, records):
     answer = []
+    # 차량별 출입 시간
     in_time = {}
+    # 차량별 누적 주차 시간
     parking_time = {}
+    # 23:59
     last_minutes = 23 * 60 + 59
     
     for record in records:
+        # 시간, 차량 번호, 상태
         time, number, state = record.split(' ')
         
+        # 시간 '분'으로 변환
         minutes = int(time[3:5]) + int(time[0:2]) * 60
         
-        if state == 'IN':
+        if state == 'IN':   # 입차 기록인 경우
             in_time[number] = minutes
-        else:
+        elif state == 'OUT':   # 출차 기록인 경우
             if number in parking_time:
                 parking_time[number] += minutes - in_time[number]
                 in_time.pop(number)
@@ -21,15 +26,18 @@ def solution(fees, records):
                 parking_time[number] = minutes - in_time[number]
                 in_time.pop(number)
     
+    # 출차된 내역 없는 기록 정리
     for number, in_minutes in in_time.items():
         if number in parking_time:
             parking_time[number] += last_minutes - in_minutes
         else:
             parking_time[number] = last_minutes - in_minutes
     
+    # 차량 번호에 따라 정렬
     pt_array = list(parking_time.items())
     pt_array.sort(key=lambda x:x[0])
     
+    # 요금 계산
     for number, pt_minutes in pt_array:
         if pt_minutes <= fees[0]:
             answer.append(fees[1])
