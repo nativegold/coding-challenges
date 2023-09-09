34bd7a5f5ef591ab75ff5dbf0549b0c9ce4b03e1
Dongwoon Kim <kdw0410@outlook.com>
2023-09-09 17:31:39 +0900

    [level 2] Title: 괄호 변환, Time: 0.93 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.py
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.py
@@ -1,35 +1,36 @@
 from collections import deque
 
-def make_correct_string(s):
-    if s == '':
+def make_correct_parentheses(s):
+    if s == '':  # 빈 문자열인 경우 그대로 반환.
         return ''
     
     u = ''
     v = deque(s)
     
-    while v:
+    while v:  # v가 빌 때까지 반복
         u += v.popleft()
         
+        # u에 있는 '('와 ')'의 개수가 같아질 때
         if u.count('(') == u.count(')'):
-            new_str = ''
-            start_count = 0
-            end_count = 0
+            start_count = 0  # '('의 개수를 저장
+            end_count = 0  # ')'의 개수를 저장
         
+            # u 안의 문자들을 하나씩 검사
             for c in u:
                 if c == '(':
-                    start_count += 1
-                    new_str += ')'
+                    start_count += 1  # '('를 발견할 때마다 start_count를 1 증가
                 elif c == ')':
-                    end_count += 1
-                    new_str += '('
+                    end_count += 1  # ')'를 발견할 때마다 end_count를 1 증가.
                     
-                print(new_str)
-                    
+                # '('와 ')'의 개수가 같을 때
                 if start_count == end_count:
-                    return u + make_correct_string(''.join(v))
+                    # 현재의 u와 재귀적으로 v를 올바른 괄호 문자열로 만든 값을 더해 반환
+                    return u + make_correct_parentheses(''.join(v))
+                # ')'의 개수가 더 많을 때
                 elif start_count < end_count:
-                    return '(' + make_correct_string(''.join(v)) + ')' + new_str[1:-1] 
-                
-    
+                    # 문제에서 정의한 방법에 따라 문자열을 재배열하고 반환
+                    return '(' + make_correct_parentheses(''.join(v)) + ')' + \
+                            u[1:-1].replace('(','0').replace(')','(').replace('0',')')
+
 def solution(p):
-    return make_correct_string(p)
+    return make_correct_parentheses(p)
