37dd70284b1ba1ac95979108f1198c0204cb69ff
Dongwoon Kim <kdw0410@outlook.com>
2023-09-09 15:28:51 +0900

    [level 2] Title: 괄호 변환, Time: 0.88 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.py
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-def make_correct_parentheses(s):
+def make_correct_string(s):
     if s == '':
         return ''
     
@@ -11,21 +11,25 @@ def make_correct_parentheses(s):
         u += v.popleft()
         
         if u.count('(') == u.count(')'):
+            new_str = ''
             start_count = 0
             end_count = 0
         
             for c in u:
                 if c == '(':
                     start_count += 1
+                    new_str += ')'
                 elif c == ')':
                     end_count += 1
+                    new_str += '('
+                    
+                print(new_str)
                     
                 if start_count == end_count:
-                    return u + make_correct_parentheses(''.join(v))
+                    return u + make_correct_string(''.join(v))
                 elif start_count < end_count:
-                    return '(' + make_correct_parentheses(''.join(v)) + ')' + \
-                            u[1:-1].replace('(','0').replace(')','(').replace('0',')')
+                    return '(' + make_correct_string(''.join(v)) + ')' + new_str[1:-1] 
                 
     
 def solution(p):
-    return make_correct_parentheses(p)
+    return make_correct_string(p)
