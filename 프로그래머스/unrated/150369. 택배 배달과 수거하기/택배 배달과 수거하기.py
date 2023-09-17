faa0326dd97138a74532e92dd76a74315eb10d63
Dongwoon Kim <kdw0410@outlook.com>
2023-09-17 17:29:37 +0900

    [unrated] Title: 택배 배달과 수거하기, Time: 21.47 ms, Memory: 11.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+def solution(cap, n, deliveries, pickups):
+    answer = 0
+    deliver = 0
+    pickup = 0
+
+    for i in range(n-1, -1, -1):
+        deliver += deliveries[i]
+        pickup += pickups[i]
+
+        while deliver > 0 or pickup > 0:
+            deliver -= cap
+            pickup -= cap
+            answer += (i + 1) * 2
+
+    return answer
