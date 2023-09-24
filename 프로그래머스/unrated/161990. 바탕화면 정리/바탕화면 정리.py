f4960974ee8d96d04d4adde117dda77aac3c5899
Dongwoon Kim <kdw0410@outlook.com>
2023-09-24 14:32:46 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.16 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,17 +1,15 @@
 def solution(wallpaper):
-    min_height = float('inf')
-    min_width = float('inf')
-    max_height = 0
-    max_width = 0
+    start_height = float('inf')
+    start_width = float('inf')
+    end_height = 0
+    end_width = 0
     
     for i in range(len(wallpaper)):
         for j in range(len(wallpaper[i])):
             if wallpaper[i][j] == '#':
-                min_height = i if i < min_height else min_height
-                min_width = j if j < min_width else min_width
-                max_height = i+1 if i+1 > max_height else max_height
-                max_width = j+1 if j+1 > max_width else max_width
-    
-    
+                start_height = i if i < start_height else start_height
+                start_width = j if j < start_width else start_width
+                end_height = i+1 if i+1 > end_height else end_height
+                end_width = j+1 if j+1 > end_width else end_width
 
-    return [min_height, min_width, max_height, max_width]
+    return [start_height, start_width, end_height, end_width]
