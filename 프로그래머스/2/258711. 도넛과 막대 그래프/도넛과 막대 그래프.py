a34a664511e690ae46b6a52072f4c8b905f6c220
Dongwoon Kim <kdw0410@outlook.com>
2025-01-21 20:17:41 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 2105.89 ms, Memory: 336 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -19,7 +19,7 @@ def solution(edges):
             graph[x1] = set()
         graph[x1].add(x2)
     
-    generated_node = 0   # 중간에서 연결한 정점
+    generated_node = 0   # 그래프끼리 연결하기 위해 생성한 정점
     
     for node, edge_count in node_edge_count.items():
         if edge_count[0] >= 2 and edge_count[1] == 0:
@@ -28,9 +28,9 @@ def solution(edges):
     
     answer[0] = generated_node
     
-    visited = set()
+    visited = set()     # 방문한 노드
 
-    for first_node in graph[generated_node]:
+    for first_node in graph[generated_node]:    # 생성한 정점이 연결한 그래프의 첫번째 노드들
         if first_node not in graph:
             answer[2] += 1
             continue
@@ -41,31 +41,31 @@ def solution(edges):
         
         queue = deque(graph[first_node])
             
-        has_two_edges = False
-        first_node_destination_count = 0
+        has_two_edges = False   # 다른 정점으로 향하는 2개의 간선 이상을 가지고 있는 경우
+        has_edge_from_node = False
         
-        while queue:
+        while queue:    # BFS
             current_node = queue.popleft()
             visited.add(current_node)
             
             if current_node not in graph:
                 break
                 
-            if len(graph[current_node]) >= 2:
+            if len(graph[current_node]) >= 2:   # 한 정점이 간선 두 개를 가지고 있는 경우 --> 8자 모양 그래프
                 has_two_edges = True
                 break
             
             for next_node in graph[current_node]:
                 if next_node == first_node:
-                    first_node_destination_count += 1
+                    has_edge_from_node = True
                 if next_node not in visited:
                     queue.append(next_node)
 
         if has_two_edges:   # 8자 모양 그래프
             answer[3] += 1
-        elif first_node_destination_count == 1:     # 도넛 모양 그래프
+        elif has_edge_from_node:     # 도넛 모양 그래프
             answer[1] += 1
-        elif first_node_destination_count == 0:     # 막대 모양 그래프
+        elif not has_edge_from_node:     # 막대 모양 그래프
             answer[2] += 1
 
     return answer
